32ddffd1a9f2b3a9258942c735b85a8d79019ad4
Karan Sahu <iamironman892465@gmail.com>
2025-10-05 23:20:16 +0530

    second commit

---
+++ backend/src/index.ts
@@ -0,0 +1,14 @@
+import  {WebSocketServer} from  "ws";
+//creating websocket server which runs on followed port
+const wss = new WebSocketServer({port : 8080});
+//event handler 
+//wss par jab bhi koi naya connection aaye toh take it here->
+wss.on('connection',(socket) =>{
+  console.log("user connected");
+    socket.on("message" , e => {
+        console.log(e.toString());
+  if(e.toString() == "ping") socket.send("pong");
+    })
+})
+
+
